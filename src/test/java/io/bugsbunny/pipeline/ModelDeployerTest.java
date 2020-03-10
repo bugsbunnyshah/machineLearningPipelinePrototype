c473f3f0b387e6589bda9750dd539449de4738da
babyboy <bugs.bunny.shah@gmail.com>
2020-03-10 16:55:16 -0500

    restarting_my_baby_from_four_months_ago

--- src/test/java/io/bugsbunny/pipeline/ModelDeployerTest.java
+++ src/test/java/io/bugsbunny/pipeline/ModelDeployerTest.java
@@ -1,16 +1,18 @@
 package io.bugsbunny.pipeline;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jdk.nashorn.internal.ir.FunctionNode;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.junit.jupiter.api.Test;
+import org.kamranzafar.jtar.TarEntry;
+import org.kamranzafar.jtar.TarInputStream;
+import org.kamranzafar.jtar.TarOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class ModelDeployerTest {
     private static Logger logger = LoggerFactory.getLogger(ModelDeployerTest.class);
@@ -28,19 +30,17 @@ public class ModelDeployerTest {
     @Test
     public void testReadTarFile() throws Exception
     {
-        TarArchiveInputStream tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(
-                new FileInputStream("/Users/babyboy/mamasboy/appgallabsForProfit/machineLearningPipelinePrototype/src/main/resources/code-with-quarkus-jvm.tar.gz")));
-        TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
-        BufferedReader br = null;
-        StringBuilder sb = new StringBuilder();
-        while (currentEntry != null) {
-            br = new BufferedReader(new InputStreamReader(tarInput)); // Read directly from tarInput
-            System.out.println("For File = " + currentEntry.getName());
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println("line="+line);
-            }
-            currentEntry = tarInput.getNextTarEntry(); // You forgot to iterate to the next file
+        String tarFile = "/Users/babyboy/mamasboy/appgallabsForProfit/machineLearningPipelinePrototype/src/main/resources/code-with-quarkus-jvm.tar";
+        String destFolder = "target/code-with-quarkus-jvm.docker";
+
+        TarInputStream tis = new TarInputStream(new BufferedInputStream(new FileInputStream(tarFile)));
+        TarEntry entry;
+        while((entry = tis.getNextEntry()) != null)
+        {
+            int count;
+            byte data[] = new byte[2048];
         }
+
+        tis.close();
     }
 }
