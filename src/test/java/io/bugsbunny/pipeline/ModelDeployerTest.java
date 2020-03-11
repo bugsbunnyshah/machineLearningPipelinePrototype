cc5ca75001913d6b04a34d447b251eb7ae94dd08
babyboy <bugs.bunny.shah@gmail.com>
2020-03-11 11:06:41 -0500

    restarting_my_baby_from_four_months_ago

--- src/test/java/io/bugsbunny/pipeline/ModelDeployerTest.java
+++ src/test/java/io/bugsbunny/pipeline/ModelDeployerTest.java
@@ -1,10 +1,12 @@
 package io.bugsbunny.pipeline;
 
+import com.google.common.base.Charsets;
 import io.quarkus.test.junit.QuarkusTest;
 import jdk.nashorn.internal.ir.FunctionNode;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.kamranzafar.jtar.TarEntry;
 import org.kamranzafar.jtar.TarInputStream;
@@ -13,6 +15,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 public class ModelDeployerTest {
     private static Logger logger = LoggerFactory.getLogger(ModelDeployerTest.class);
@@ -32,15 +40,23 @@ public class ModelDeployerTest {
     {
         String tarFile = "/Users/babyboy/mamasboy/appgallabsForProfit/machineLearningPipelinePrototype/src/main/resources/code-with-quarkus-jvm.tar";
         String destFolder = "target/code-with-quarkus-jvm.docker";
+        File file = new File(tarFile);
+        int fileLength = (int)file.length();
 
-        TarInputStream tis = new TarInputStream(new BufferedInputStream(new FileInputStream(tarFile)));
-        TarEntry entry;
-        while((entry = tis.getNextEntry()) != null)
-        {
-            int count;
-            byte data[] = new byte[2048];
+        try (TarArchiveInputStream fin = new TarArchiveInputStream(new FileInputStream(file))){
+            System.out.println("BLAHBLAH");
+            TarArchiveEntry entry = fin.getCurrentEntry();
+            TarArchiveEntry nextEntry = null;
+            do {
+                if(entry != null) {
+                    System.out.println("Current File: " + entry.getName());
+                    entry = null;
+                }
+                nextEntry = fin.getNextTarEntry();
+                if(nextEntry != null) {
+                    System.out.println("Next File: " + nextEntry.getName());
+                }
+            }while(nextEntry != null);
         }
-
-        tis.close();
     }
 }
