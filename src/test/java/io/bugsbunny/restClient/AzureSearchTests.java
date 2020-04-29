80882e54c9a7a5bdd9b773271d4ab9eed984ad20
babyboy <bugs.bunny.shah@gmail.com>
2020-04-29 17:03:39 -0500

    azSearch > Create index, Create Indexer

--- src/test/java/io/bugsbunny/restClient/AzureSearchTests.java
+++ src/test/java/io/bugsbunny/restClient/AzureSearchTests.java
@@ -32,13 +32,13 @@ public class AzureSearchTests {
     private static Logger logger = LoggerFactory.getLogger(AzureSearchTests.class);
 
     @Test
-    public void test() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException {
+    public void testCreateDataSource() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException {
         String requestUrl = "https://alz.search.windows.net/datasources?api-version=2019-05-06";
         String blobContainerName = "alzBlobContainer";
         String accountName = "alzAccount";
         String primaryKey = "CB509E0E7E50948D4CCAC506BD5655F5";
         String json = "{\n" +
-                "  \"name\" : \"cog-search-demo-ds\",\n" +
+                "  \"name\" : \"alzds\",\n" +
                 "  \"description\" : \"Demo files to demonstrate cognitive search capabilities.\",\n" +
                 "  \"type\" : \"azureblob\",\n" +
                 "  \"credentials\" :\n" +
@@ -60,6 +60,269 @@ public class AzureSearchTests {
                 .header("Content-Type", "application/json")
                 .header("api-key",primaryKey)
                 .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String tokenJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        logger.info("*******");
+        logger.info(status+"");
+        logger.info(tokenJson);
+        logger.info("*******");
+    }
+
+    @Test
+    public void testCreateSkillSet() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException {
+        String requestUrl = "https://alz.search.windows.net/skillsets/alzds?api-version=2019-05-06";
+        String blobContainerName = "alzBlobContainer";
+        String accountName = "alzAccount";
+        String primaryKey = "CB509E0E7E50948D4CCAC506BD5655F5";
+        String json = "{\n" +
+                "  \"description\": \"Extract entities, detect language and extract key-phrases\",\n" +
+                "  \"skills\":\n" +
+                "  [\n" +
+                "    {\n" +
+                "      \"@odata.type\": \"#Microsoft.Skills.Text.EntityRecognitionSkill\",\n" +
+                "      \"categories\": [ \"Person\", \"Organization\", \"Location\" ],\n" +
+                "      \"defaultLanguageCode\": \"en\",\n" +
+                "      \"inputs\": [\n" +
+                "        { \"name\": \"text\", \"source\": \"/document/content\" }\n" +
+                "      ],\n" +
+                "      \"outputs\": [\n" +
+                "        { \"name\": \"persons\", \"targetName\": \"persons\" },\n" +
+                "        { \"name\": \"organizations\", \"targetName\": \"organizations\" },\n" +
+                "        { \"name\": \"locations\", \"targetName\": \"locations\" }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"@odata.type\": \"#Microsoft.Skills.Text.LanguageDetectionSkill\",\n" +
+                "      \"inputs\": [\n" +
+                "        { \"name\": \"text\", \"source\": \"/document/content\" }\n" +
+                "      ],\n" +
+                "      \"outputs\": [\n" +
+                "        { \"name\": \"languageCode\", \"targetName\": \"languageCode\" }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"@odata.type\": \"#Microsoft.Skills.Text.SplitSkill\",\n" +
+                "      \"textSplitMode\" : \"pages\",\n" +
+                "      \"maximumPageLength\": 4000,\n" +
+                "      \"inputs\": [\n" +
+                "        { \"name\": \"text\", \"source\": \"/document/content\" },\n" +
+                "        { \"name\": \"languageCode\", \"source\": \"/document/languageCode\" }\n" +
+                "      ],\n" +
+                "      \"outputs\": [\n" +
+                "        { \"name\": \"textItems\", \"targetName\": \"pages\" }\n" +
+                "      ]\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"@odata.type\": \"#Microsoft.Skills.Text.KeyPhraseExtractionSkill\",\n" +
+                "      \"context\": \"/document/pages/*\",\n" +
+                "      \"inputs\": [\n" +
+                "        { \"name\": \"text\", \"source\": \"/document/pages/*\" },\n" +
+                "        { \"name\":\"languageCode\", \"source\": \"/document/languageCode\" }\n" +
+                "      ],\n" +
+                "      \"outputs\": [\n" +
+                "        { \"name\": \"keyPhrases\", \"targetName\": \"keyPhrases\" }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        X509TrustManager xtm = new AzureSearchTests.MyTrustManager();
+        TrustManager mytm[] = {xtm};
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        ctx.init(null,mytm, null );
+        SSLSocketFactory sf = ctx.getSocketFactory();
+        HttpClient httpClient = HttpClient.newBuilder().sslContext(ctx).build();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(requestUrl))
+                .header("Content-Type", "application/json")
+                .header("api-key",primaryKey)
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String tokenJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        logger.info("*******");
+        logger.info(status+"");
+        logger.info(tokenJson);
+        logger.info("*******");
+    }
+
+    @Test
+    public void testCreateIndex() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException {
+        String requestUrl = "https://alz.search.windows.net/indexes/azlindex?api-version=2019-05-06";
+        String blobContainerName = "alzBlobContainer";
+        String accountName = "alzAccount";
+        String primaryKey = "CB509E0E7E50948D4CCAC506BD5655F5";
+        String json = "{\n" +
+                "  \"fields\": [\n" +
+                "    {\n" +
+                "      \"name\": \"id\",\n" +
+                "      \"type\": \"Edm.String\",\n" +
+                "      \"key\": true,\n" +
+                "      \"searchable\": true,\n" +
+                "      \"filterable\": false,\n" +
+                "      \"facetable\": false,\n" +
+                "      \"sortable\": true\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"metadata_storage_name\",\n" +
+                "      \"type\": \"Edm.String\",\n" +
+                "      \"searchable\": false,\n" +
+                "      \"filterable\": false,\n" +
+                "      \"facetable\": false,\n" +
+                "      \"sortable\": false\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"content\",\n" +
+                "      \"type\": \"Edm.String\",\n" +
+                "      \"sortable\": false,\n" +
+                "      \"searchable\": true,\n" +
+                "      \"filterable\": false,\n" +
+                "      \"facetable\": false\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"languageCode\",\n" +
+                "      \"type\": \"Edm.String\",\n" +
+                "      \"searchable\": true,\n" +
+                "      \"filterable\": false,\n" +
+                "      \"facetable\": false\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"keyPhrases\",\n" +
+                "      \"type\": \"Collection(Edm.String)\",\n" +
+                "      \"searchable\": true,\n" +
+                "      \"filterable\": false,\n" +
+                "      \"facetable\": false\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"persons\",\n" +
+                "      \"type\": \"Collection(Edm.String)\",\n" +
+                "      \"searchable\": true,\n" +
+                "      \"sortable\": false,\n" +
+                "      \"filterable\": true,\n" +
+                "      \"facetable\": true\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"organizations\",\n" +
+                "      \"type\": \"Collection(Edm.String)\",\n" +
+                "      \"searchable\": true,\n" +
+                "      \"sortable\": false,\n" +
+                "      \"filterable\": true,\n" +
+                "      \"facetable\": true\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"locations\",\n" +
+                "      \"type\": \"Collection(Edm.String)\",\n" +
+                "      \"searchable\": true,\n" +
+                "      \"sortable\": false,\n" +
+                "      \"filterable\": true,\n" +
+                "      \"facetable\": true\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        X509TrustManager xtm = new AzureSearchTests.MyTrustManager();
+        TrustManager mytm[] = {xtm};
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        ctx.init(null,mytm, null );
+        SSLSocketFactory sf = ctx.getSocketFactory();
+        HttpClient httpClient = HttpClient.newBuilder().sslContext(ctx).build();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(requestUrl))
+                .header("Content-Type", "application/json")
+                .header("api-key",primaryKey)
+                .PUT(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String tokenJson = httpResponse.body();
+        int status = httpResponse.statusCode();
+        logger.info("*******");
+        logger.info(status+"");
+        logger.info(tokenJson);
+        logger.info("*******");
+    }
+
+    @Test
+    public void testCreateIndexer() throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, InterruptedException {
+        String requestUrl = "https://alz.search.windows.net/indexes/azlindexer?api-version=2019-05-06";
+        String blobContainerName = "alzBlobContainer";
+        String accountName = "alzAccount";
+        String primaryKey = "CB509E0E7E50948D4CCAC506BD5655F5";
+        String json = "{\n" +
+                "  \"name\":\"azlindexer\",\n" +
+                "  \"dataSourceName\" : \"alzds\",\n" +
+                "  \"targetIndexName\" : \"azlindex\",\n" +
+                "  \"fieldMappings\" : [\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"metadata_storage_path\",\n" +
+                "      \"targetFieldName\" : \"id\",\n" +
+                "      \"mappingFunction\" :\n" +
+                "      { \"name\" : \"base64Encode\" }\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"metadata_storage_name\",\n" +
+                "      \"targetFieldName\" : \"metadata_storage_name\",\n" +
+                "      \"mappingFunction\" :\n" +
+                "      { \"name\" : \"base64Encode\" }\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"content\",\n" +
+                "      \"targetFieldName\" : \"content\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"outputFieldMappings\" :\n" +
+                "  [\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"/document/persons\",\n" +
+                "      \"targetFieldName\" : \"persons\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"/document/organizations\",\n" +
+                "      \"targetFieldName\" : \"organizations\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"/document/locations\",\n" +
+                "      \"targetFieldName\" : \"locations\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\" : \"/document/pages/*/keyPhrases/*\",\n" +
+                "      \"targetFieldName\" : \"keyPhrases\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"sourceFieldName\": \"/document/languageCode\",\n" +
+                "      \"targetFieldName\": \"languageCode\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"parameters\":\n" +
+                "  {\n" +
+                "    \"maxFailedItems\":-1,\n" +
+                "    \"maxFailedItemsPerBatch\":-1,\n" +
+                "    \"configuration\":\n" +
+                "    {\n" +
+                "      \"dataToExtract\": \"contentAndMetadata\",\n" +
+                "      \"parsingMode\": \"default\",\n" +
+                "      \"firstLineContainsHeaders\": false,\n" +
+                "      \"delimitedTextDelimiter\": \",\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        X509TrustManager xtm = new AzureSearchTests.MyTrustManager();
+        TrustManager mytm[] = {xtm};
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        ctx.init(null,mytm, null );
+        SSLSocketFactory sf = ctx.getSocketFactory();
+        HttpClient httpClient = HttpClient.newBuilder().sslContext(ctx).build();
+
+        HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder();
+        HttpRequest httpRequest = httpRequestBuilder.uri(new URI(requestUrl))
+                .header("Content-Type", "application/json")
+                .header("api-key",primaryKey)
+                .PUT(HttpRequest.BodyPublishers.ofString(json.toString()))
                 .build();
         HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
         String tokenJson = httpResponse.body();
