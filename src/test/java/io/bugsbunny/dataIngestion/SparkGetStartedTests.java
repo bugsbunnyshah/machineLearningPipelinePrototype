ae97ef000993a1d5d4ca15878c38bc52c147d1b6
babyboy <bugs.bunny.shah@gmail.com>
2020-05-19 09:28:16 -0500

    starting_data_transformation_research_using_spark

--- src/test/java/io/bugsbunny/dataIngestion/SparkGetStartedTests.java
+++ src/test/java/io/bugsbunny/dataIngestion/SparkGetStartedTests.java
@@ -5,20 +5,23 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
 @QuarkusTest
-public class SparkGetStartedTests {
+public class SparkGetStartedTests implements Serializable {
     private static Logger logger = LoggerFactory.getLogger(SparkGetStartedTests.class);
 
-    @Test
+    //@Test
     public void testFirstStart() throws Exception
     {
         String logFile = "README.md"; // Should be some file on your system
@@ -48,5 +51,25 @@ public class SparkGetStartedTests {
 
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data);
+
+        final Integer sum = distData.reduce((a, b) -> a + b);
+
+        logger.info("*******");
+        logger.info("Sum is: "+sum);
+        logger.info("*******");
+
+        sc.stop();
+    }
+
+    @Test
+    public void testMapReduce() throws Exception
+    {
+        Skills skills = new Skills();
+
+        int lineCount = skills.countLines("skills.json");
+
+        logger.info("*******");
+        logger.info("Total Length: "+lineCount);
+        logger.info("*******");
     }
 }
